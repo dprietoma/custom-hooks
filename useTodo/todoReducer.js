aa6362d09e8b685f2522d2f6985ec80ab0318606
David Prieto <davidfelipeprieto@gmail.com>
2024-03-19 18:33:07 -0500

    useTodo agregado

---
+++ useTodo/todoReducer.js
@@ -0,0 +1,24 @@
+export const todoReducer = ( initialState, action) => {
+
+    switch (action.type) {
+        case '[Todo] Add': 
+            return [...initialState, action.payload];
+        
+        case '[Todo] Remove':  
+            return initialState.filter(todo => todo.id !== action.payload);
+        
+        case '[Todo] Toggle':
+            return initialState.map( todo => {
+                if (todo.id === action.payload) {
+                    return {
+                        ...todo,
+                        done: !todo.done
+                    }
+                }
+                return todo;
+            });
+    
+        default:
+            return initialState;
+    }
+} 
